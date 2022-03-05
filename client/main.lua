3501fa569a2a90a90efff4c3d1e4f5331d343192
MackeB <antisideebuissneiss@gmail.com>
2022-03-05 13:16:20 +0100

    lots of fixes, new code and functions
    there's a couple of bugs atm, will be solved soon

--- client/main.lua
+++ client/main.lua
@@ -5,6 +5,19 @@ local ped = PlayerPedId()
 local door = 277255495
 local door2 = 0
 
+function PlayerOpenDoor()
+  SetVehicleDoorOpen(car, 0, false, false)
+  Wait(10000)
+  TriggerEvent('carscrap:client:doAnimation')
+  SetVehicleDoorBroken(car, 0, true)
+end
+
+function SpawnDoor()
+  door2 = CreateObject(door, -1155.83, -2007.33, 13.18, true, false, false)
+  AttachEntityToEntity(door2, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 57005), 0.14, 0, -0.01, 90.0, -90.0, 180.0, true, true, false, true, 1, true)
+  print('yes')
+end
+
 
 CreateThread(function()
   exports['qb-target']:AddBoxZone("name", vector3(-1140.88, -1987.93, 13.17), 1, 1, { 
@@ -17,7 +30,7 @@ CreateThread(function()
     options = { 
       { 
         type = "client", 
-        event = "carpart:client:spawnCar", 
+        event = "carscrap:client:spawnCar", 
         icon = "fas fa-search",
         label = 'Talk To Jake', 
       }
@@ -26,29 +39,113 @@ CreateThread(function()
   })
 end)
 
-RegisterNetEvent('carpart:client:spawnCar', function()
+RegisterNetEvent('carscrap:client:spawnCar', function()
     if missionStarted then
         QBCore.Functions.Notify('You Are Already Working', 'error', 5000) 
         return
     end
 
     missionStarted = true
-    TriggerEvent('carpart:client:startJob')
+    TriggerEvent('carscrap:client:startJob')
 
     QBCore.Functions.SpawnVehicle('adder', function(veh)
       car = veh
     end, vector4(-1157.11, -2007.35, 13.18, 173.24), true)
 end)
 
-RegisterNetEvent('carpart:client:startJob', function()
+RegisterNetEvent('carscrap:client:startJob', function()
   QBCore.Functions.Notify('Go Inside The Building', 'primary', 5000)
-  TriggerEvent('carpart:client:removeDoors')
+  TriggerEvent('carscrap:client:removeDoors')
 end)
 
-RegisterNetEvent('carpart:client:endJob', function()
+RegisterNetEvent('carscrap:client:endJob', function()
   missionStarted = false
   
 end)
+
+RegisterNetEvent('carscrap:client:removeDoors', function()
+carZone = CircleZone:Create(vector3(-1157.45, -2007.6, 13.18), 3.0, {
+  name="s",
+  useZ=false,
+  debugPoly=true
+})
+    carZone:onPlayerInOut(function(isPointInside)
+      print('Tomato')
+      TriggerEvent('carscrap:client:circleCarZone')
+    end)
+end)
+
+RegisterNetEvent('carscrap:client:circleCarZone', function()
+    QBCore.Functions.Notify('Remove The Doors', 'primary', 5000)
+      TaskGoStraightToCoord(ped, -1155.84, -2007.33, 13.18, 138.98, 15.0, -1, 0.0, 0.0)
+      print('damnit')
+    Wait(5000)
+    FreezeEntityPosition(ped, true)
+    PlayerOpenDoor()
+    Wait(6000)
+    PolyZone:destroy()
+end)
+
+RegisterNetEvent('carscrap:client:doAnimation', function()
+QBCore.Functions.Progressbar("doorRemoval", 'Removing the door', 5500, false, true, {
+        disableMovement = true,
+        disableCarMovement = true,
+        disableMouse = false,
+        disableCombat = true,
+    }, {
+        animDict = "mini@repair",
+        anim = "fixing_a_ped",
+        flags = 16,
+    }, {}, {}, function() -- Done
+        StopAnimTask(PlayerPedId(), "mini@repair", "fixing_a_ped", 1.0)
+            SpawnDoor()
+            FreezeEntityPosition(ped, false)
+              QBCore.Functions.Notify('Go To The Shelfs On Your Left', 'primary', 5000) 
+            TriggerEvent('carscrap:client:deliverBox')
+    end, function() -- Cancel
+        StopAnimTask(PlayerPedId(), "mini@repair", "fixing_a_ped", 1.0)
+        FreezeEntityPosition(ped, false)
+        QBCore.Functions.Notify('Canceled', "error")
+    end)
+end)
+
+RegisterNetEvent('carscrap:client:deliverBox', function()
+deliver = BoxZone:Create(vector3(-1145.34, -2014.98, 13.18), 15.8, 5, {
+  name="box",
+  heading=315,
+  debugPoly=true,
+  minZ = 12.7, 
+  maxZ = 16.9, 
+})
+deliver:onPlayerInOut(function(isPointInside)
+TriggerServerEvent('carscrap:server:giveRewards')
+
+end)
+
+RegisterNetEvent('carscrap:client:finishScrap', function()
+  QBCore.Functions.Progressbar('deliverGoods', 'Putting door on shelf', 5000, false, true, { -- Name | Label | Time | useWhileDead | canCancel
+      disableMovement = true,
+      disableCarMovement = true,
+      disableMouse = false,
+      disableCombat = true,
+  }, {
+      animDict = 'anim@gangops@facility@servers@',
+      anim = 'hotwire',
+      flags = 16,
+  }, {}, {}, function() -- Play When Done
+      TriggerServerEvent('carscrap:server:giveRewards')
+  end, function() -- Play When Cancel
+      QBCore.Functions.Notify('You canceled', error, 5000) 
+  end)
+end)
+
+RegisterNetEvent('carscrap:client:devTools', function()
+  DetachEntity(door2, false, false)
+  DeleteEntity(door2)
+  ClearPedTasksImmediately(ped)
+  end)
+end)
+
 
 CreateThread(function()
     carblip = AddBlipForCoord(vector3(-1140.11, -1988.37, 13.17))
@@ -61,45 +158,3 @@ CreateThread(function()
     AddTextComponentSubstringPlayerName("Car Scrapping")
     EndTextCommandSetBlipName(carblip)
 end)
-
-RegisterNetEvent('carpart:client:removeDoors', function()
-carZone = CircleZone:Create(vector3(-1157.45, -2007.6, 13.18), 3.0, {
-  name="s",
-  useZ=false,
-  debugPoly=true
-})
-    carZone:onPlayerInOut(function(isPointInside)
-      print('Tomato')
-      TriggerEvent('carpart:client:circleCarZone')
-    end)
-end)
-
-RegisterNetEvent('carpart:client:circleCarZone', function()
-    QBCore.Functions.Notify('Remove The Doors', 'primary', 5000)
-      TaskGoStraightToCoord(ped, -1155.84, -2007.33, 13.18, 138.98, 15.0, -1, 0.0, 0.0)
-    Wait(100)
-    PlayerOpenDoor()
-    Wait(2000)
-    SpawnDoor()
-    Wait(6000)
-    PolyZone:destroy()
-end)
-
-function SpawnDoor()
-  door2 = CreateObject(door, -1155.83, -2007.33, 13.18, true, false, false)
-  AttachEntityToEntity(door2, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 57005), 0.14, 0, -0.01, 90.0, -90.0, 180.0, true, true, false, true, 1, true)
-  print('yes')
-end
-
-function PlayerOpenDoor()
-  print(ped)
-  print(car)
-  TaskOpenVehicleDoor(ped, car, 1, -1, 20)
-end
-
-RegisterNetEvent('carscrap:client:devTools', function()
-  DetachEntity(door2, false, false)
-  DeleteEntity(door2)
-  ClearPedTasksImmediately(ped)
-end)
-
--- server/main.lua
+++ server/main.lua
@@ -4,3 +4,8 @@ QBCore.Commands.Add("damn", "Give Random Items (God Only)", {}, false, function(
     TriggerClientEvent('carscrap:client:devTools', source)
 end)
 
+RegisterNetEvent('carscrap:server:giveRewards', function()
+    Player.Functions.AddMoney('cash', 125)
+    TriggerClientEvent('carscrap:client:devTools')
+    QBCore.Functions.Notify('You Got $125 From Scrapping The Car', 'success', 5000) 
+end)
